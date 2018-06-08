5bdc46fe226bd9c98689c29c159c1db27698ba18
Jeremy12345 <jeremyngelbrecht@gmail.com>
2018-06-08 12:28:30 +0200

    trying something

--- MyBucks.Mvc.Tools/ApiController.cs
+++ MyBucks.Mvc.Tools/ApiController.cs
@@ -1,63 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using MyBucks.Core.Model;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using MyBucks.Core.Model;
 
 namespace MyBucks.Mvc.Tools
 {
-    public class ApiController : ControllerBase
+    public class ApiController //: ControllerBase
     {
 
-        public BadRequestObjectResult BadRequest(string message)
-        {
-            return BadRequest(new ApiResponse(message));
-        }
-
-        public ObjectResult InternalServerError(string message)
-        {
-            return new ObjectResult(new ApiResponse(message));
-        }
-
-        public ObjectResult Unauthorized(string message)
-        {
-            return new ObjectResult(new ApiResponse(message));
-        }
-
-        // todo: more methods
-
-        public IActionResult FromReply(ReplyBase reply)
-        {
-            if (reply.ReplyStatus == ReplyStatus.Failed)
-            {
-                return InternalServerError(reply.ReplyMessage);
-            }
-
-            if (reply.ReplyStatus == ReplyStatus.NotFound)
-            {
-                return NotFound(reply.ReplyMessage);
-            }
-
-            return Ok();
-        }
-
-        public IActionResult FromReply<TData>(ReplyBase reply, TData data)
-        {
-            if (reply.ReplyStatus == ReplyStatus.Failed)
-            {
-                return InternalServerError(reply.ReplyMessage);
-            }
-
-            if (reply.ReplyStatus == ReplyStatus.NotFound)
-            {
-                return NotFound(reply.ReplyMessage);
-            }
-
-            if (reply.ReplyStatus == ReplyStatus.Unauthorized)
-            {
-                return Unauthorized(reply.ReplyMessage);
-            }
-
-            return Ok(data);
-
-        }
+//        public BadRequestObjectResult BadRequest(string message)
+//        {
+//            return BadRequest(new ApiResponse(message));
+//        }
+//
+//        public ObjectResult InternalServerError(string message)
+//        {
+//            return new ObjectResult(new ApiResponse(message));
+//        }
+//
+//        public ObjectResult Unauthorized(string message)
+//        {
+//            return new ObjectResult(new ApiResponse(message));
+//        }
+//
+//        // todo: more methods
+//
+//        public IActionResult FromReply(ReplyBase reply)
+//        {
+//            if (reply.ReplyStatus == ReplyStatus.Failed)
+//            {
+//                return InternalServerError(reply.ReplyMessage);
+//            }
+//
+//            if (reply.ReplyStatus == ReplyStatus.NotFound)
+//            {
+//                return NotFound(reply.ReplyMessage);
+//            }
+//
+//            return Ok();
+//        }
+//
+//        public IActionResult FromReply<TData>(ReplyBase reply, TData data)
+//        {
+//            if (reply.ReplyStatus == ReplyStatus.Failed)
+//            {
+//                return InternalServerError(reply.ReplyMessage);
+//            }
+//
+//            if (reply.ReplyStatus == ReplyStatus.NotFound)
+//            {
+//                return NotFound(reply.ReplyMessage);
+//            }
+//
+//            if (reply.ReplyStatus == ReplyStatus.Unauthorized)
+//            {
+//                return Unauthorized(reply.ReplyMessage);
+//            }
+//
+//            return Ok(data);
+//
+//        }
 
     }
     
