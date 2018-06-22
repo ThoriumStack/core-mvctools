0e40057125775e4e836b2d758f4d5e142b4656c1
Tjaart Blignaut <tjaartblig@gmail.com>
2018-06-22 16:30:17 +0200

    package upgrade

--- MyBucks.Mvc.Tools/Model/PaginatedResponse.cs
+++ MyBucks.Mvc.Tools/Model/PaginatedResponse.cs
@@ -4,7 +4,7 @@ namespace MyBucks.Mvc.Tools.Model
 {
     public class PaginatedResponse
     {
-        public List<object> Items { get; set; }
+        public object Items { get; set; }
         public int TotalItems { get; set; }
         public int TotalPages { get; set; }
     }
