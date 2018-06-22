6270bb70868286ed79110388bd3f02a2161bae5e
Tjaart Blignaut <tjaartblig@gmail.com>
2018-06-22 14:13:35 +0200

    add tests

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using MyBucks.Core.Model;
+using MyBucks.Mvc.Tools;
+using Xunit;
+
+namespace Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+            var apiController = new ApiController();
+
+            var lst = new ListReply<SelectItem<int>>
+            {
+                ResultList = new List<SelectItem<int>>
+                {
+                    new SelectItem<int>
+                    {
+                        Description = "asd",
+                        Id = 1
+                    },
+                    new SelectItem<int>
+                    {
+                        Description = "cxvxcv",
+                        Id = 2
+                    },
+                },
+                ReplyStatus = ReplyStatus.Successful
+            };
+
+            var result = apiController.FromReply(lst);
+            return;
+        }
+        
+        public class SelectItem<T>
+        {
+            public string Description { get; set; }
+
+            public T Id { get; set; }
+        }
+    }
+}
